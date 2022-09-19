d3dca25413c8bf28f8efd7cee7ab2e4eec0a929e
Tharun Madishetti <tharunmadishetti1@gmail.com>
2022-09-19 18:02:52 +0530

    Apartments solved

--- sorting and searching/Apartments.java
+++ sorting and searching/Apartments.java
@@ -1,29 +1,34 @@
-import java.util.*;
+import java.io.*;
+import java.io.IOException;
+import java.util.PriorityQueue;
 
 public class Apartments {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int k = sc.nextInt();
-        int[] a = new int[n];
-        int[] ap = new int[m];
-        for (int i = 0; i < n; i++)
-            a[i] = sc.nextInt();
-        for (int i = 0; i < m; i++)
-            ap[i] = sc.nextInt();
-        int c = 0, j = 0;
-        Arrays.sort(a);
-        // Arrays.sort(ap);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String st[] = br.readLine().split(" ");
+        int n = Integer.parseInt(st[0]);
+        int m = Integer.parseInt(st[1]);
+        int k = Integer.parseInt(st[2]);
+
+        st = br.readLine().split(" ");
+        PriorityQueue<Integer> req = new PriorityQueue<>(n);
         for (int i = 0; i < n; i++) {
-            if (j == ap.length)
-                break;
-            if (a[i] + k >= ap[j] && a[i] - k <= ap[j]) {
-                j++;
+            req.add(Integer.parseInt(st[i]));
+        }
+        st = br.readLine().split(" ");
+        PriorityQueue<Integer> av = new PriorityQueue<>(m);
+        for (int i = 0; i < m; i++) {
+            av.add(Integer.parseInt(st[i]));
+        }
+        int c = 0;
+        while (!req.isEmpty() && !av.isEmpty()) {
+            while (!av.isEmpty() && av.peek() < req.peek() - k)
+                av.poll();
+            if (!av.isEmpty() && av.peek() <= req.peek() + k) {
                 c++;
+                av.poll();
             }
-            if (j<m &&  a[i]-k>ap[j])
-                j++;
+            req.poll();
         }
         System.out.println(c);
     }
